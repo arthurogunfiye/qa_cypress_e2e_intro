1e1728fd41987c84a78045dd70d388ce9605d52d
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-06-30 18:02:02 +0100

    Cypress e2e intro solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="cypress" />
+import SignInPageObject from '../support/pages/signIn.pageObject';
+
+const signInPage = new SignInPageObject();
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit(signInPage.url);
+    signInPage.fillAndSubmitSignInForm(signInPage.email, signInPage.password);
+    signInPage.usernameDisplay.should('contain', signInPage.username);
   });
 });
